9c35847683a34058d2bd6618c7f6bf81d30a843b
AnnaShabatina <01.06fire@gmail.com>
2024-11-12 14:52:46 +0100

    commit

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
+function checkProbabilityTheory(count){
+  const results = []; 
+  const evennumbers = [];
+  const oddnumbers = [];
+  for(i = 0; i < count; i++){
+     var res = Math.random();
+     res *= 1000;
+     res =  Math.round(res);
+     console.log(res);
+     results.push(res);
+     if (res%2 ===0){
+        evennumbers.push(res);
+      }
+      else{
+       oddnumbers.push(res);
+      }
+   }
 
-function pow (x,y){
-  let result = 1;
-for (let i = 0; i <y; i++){
-      result  *= x; 
-  }
-  return result; 
+  const evenPercentage = (evennumbers.length/results.length)*100;
+  const oddPercentage = (oddnumbers.length/results.length)*100;
+ return  'Кількість згенерованих чисел: '+ results.length + ' \nПарних чисел: ' + evennumbers.length + '\nНе парних чисел: '+ oddnumbers.length +
+ '\nВідсоток парних до не парних: ' + evenPercentage.toFixed(2)  + ':' +  oddPercentage.toFixed(2) ;
+    
 }
+console.log(checkProbabilityTheory(8));
 
-console.log(pow(2,3));
 
 
