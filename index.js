d620e6cf777568f1b5aed9936670f58216cc6a8b
AnnaShabatina <01.06fire@gmail.com>
2024-11-13 16:39:32 +0100

    commit

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
-function checkProbabilityTheory(count){
-  const results = []; 
-  const evennumbers = [];
-  const oddnumbers = [];
-  for(i = 0; i < count; i++){
-     var res = Math.random();
-     res *= 1000;
-     res =  Math.round(res);
-     console.log(res);
-     results.push(res);
-     if (res%2 ===0){
-        evennumbers.push(res);
-      }
-      else{
-       oddnumbers.push(res);
-      }
-   }
+let lego = {
+    name: "City",
+    age: 10,
+    prise: 100,
+    numberofpieces: 2000,
+   
+   getInfo() {
+        let info = '';
+        for (let key in this) {
+            if (typeof this[key] !== 'function') { 
+                info += key + ': ' + this[key] + '\n';
+            }
+        }
+        return info;
+    }
+  }
+   
 
-  const evenPercentage = (evennumbers.length/results.length)*100;
-  const oddPercentage = (oddnumbers.length/results.length)*100;
- return  'Кількість згенерованих чисел: '+ results.length + ' \nПарних чисел: ' + evennumbers.length + '\nНе парних чисел: '+ oddnumbers.length +
- '\nВідсоток парних до не парних: ' + evenPercentage.toFixed(2)  + ':' +  oddPercentage.toFixed(2) ;
-    
-}
-console.log(checkProbabilityTheory(8));
+console.log(lego.getInfo());
 
+lego.interest = "auto";
+
+console.log(lego.getInfo());
 
 
