00a53a0ac234b84e35c9111bce279dc8e4c6ef24
AnnaShabatina <01.06fire@gmail.com>
2024-11-13 20:06:47 +0100

    commit

--- index.js
+++ index.js
@@ -1,23 +1,54 @@
-let lego = {
-    name: "City",
-    age: 10,
-    prise: 100,
-    numberofpieces: 2000,
-   
-   getInfo() {
-        let info = '';
-        for (let key in this) {
+
+var services = {
+"стрижка": "60 грн",
+"гоління": "80 грн",
+"Миття голови": "100 грн",
+  price(){
+    let pr = 0;
+       for(let key in this) {
             if (typeof this[key] !== 'function') { 
-                info += key + ': ' + this[key] + '\n';
-            }
-        }
-        return info;
-    }
+              let allPrice = parseInt(this[key].slice(0,-3));
+       
+       pr += allPrice;
+      }
   }
-console.log(lego.getInfo());
+   return "загальна вартість наданих послуг:  " + pr + " грн";
+ },
+  minPrice(){
+   const allPr = [];
+  for(let key in this) {
+            if (typeof this[key] !== 'function') { 
+              let allPrice = parseInt(this[key].slice(0,-3)); 
+              allPr.push(allPrice) ;
+            
+    }
+      
+ }
+ let minPr = Math.min(...allPr);
+ return "мінімальна ціна:  " +  minPr + " грн";
+    
+  },
+  maxPrice(){
+   const allPr = [];
+  for(let key in this) {
+            if (typeof this[key] !== 'function') { 
+              let allPrice = parseInt(this[key].slice(0,-3)); 
+              allPr.push(allPrice) ;
+            
+    }
+      
+    
+ }
+  let maxPr = Math.max(...allPr);
+  return "максімальна ціна:  " +  maxPr + " грн";
 
-lego.interest = "auto";
+  }
 
-console.log(lego.getInfo()); 
+};
+services['Розбите скло'] = "200 грн";
 
+console.log(services.price());
 
+console.log(services.minPrice());
+
+console.log(services.maxPrice());
