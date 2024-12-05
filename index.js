36e983d96190772bcfcb711d11b8220897278a0b
AnnaShabatina <01.06fire@gmail.com>
2024-12-05 18:13:23 +0100

    com

--- index.js
+++ index.js
@@ -1,54 +1,6 @@
 
-var services = {
-"стрижка": "60.66 грн",
-"гоління": "80 грн",
-"Миття голови": "100 грн",
-  price(){
-    let pr = 0;
-       for(let key in this) {
-            if (typeof this[key] !== 'function') { 
-              let allPrice = parseFloat(this[key]);
-       
-       pr += allPrice;
-      }
-  }
-   return "загальна вартість наданих послуг:  " + pr.toFixed(2) + " грн";
- },
-  minPrice(){
-   const allPr = [];
-  for(let key in this) {
-            if (typeof this[key] !== 'function') { 
-              let allPrice = parseFloat(this[key]); 
-              allPr.push(allPrice) ;
-            
-    }
-      
- }
- let minPr = Math.min(...allPr);
- return "мінімальна ціна:  " +  minPr + " грн";
-    
-  },
-  maxPrice(){
-   const allPr = [];
-  for(let key in this) {
-            if (typeof this[key] !== 'function') { 
-              let allPrice = parseFloat(this[key]); 
-              allPr.push(allPrice) ;
-            
-    }
-      
-    
- }
-  let maxPr = Math.max(...allPr);
-  return "максімальна ціна:  " +  maxPr + " грн";
+const text = "Wonderful Joyful Happiness Time Task Apple";
+const pattern = /\b(?!\w*[aA])\w{6,}\b/g;
 
-  }
-
-};
-services['Розбите скло'] = "200 грн";
-
-console.log(services.price());
-
-console.log(services.minPrice());
-
-console.log(services.maxPrice());
+const res = text.match(pattern);
+console.log(res)
